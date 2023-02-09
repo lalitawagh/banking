31d8f56fc4a8c62fb28fa9bad6a31fa39e41c023
Mohamed Ijazudeen <ijaz@kanexy.com>
2023-02-09 18:34:40 +0530

    Bug Resolved

---
+++ src/Enums/AccountEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kanexy\Banking\Enums;
+
+class AccountEnum
+{
+    public const ACTIVE = 'active';
+    public const INACTIVE = 'inactive';
+    public const CLOSED = 'closed';
+    public const PENDING = 'pending';
+    public const APPROVED = 'approved';
+    public const DECLINED = 'declined';
+}
