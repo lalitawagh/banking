a9eeb8abbb26a5bb459d40d005d2a695b428aac2
Mohamed Ijazudeen <ijaz@kanexy.com>
2023-02-09 19:42:28 +0530

    Bug Resolved

--- src/Controllers/BeneficiaryController.php
+++ src/Controllers/BeneficiaryController.php
@@ -138,6 +138,13 @@ class BeneficiaryController extends Controller
 
         $beneficiary->update($data);
 
+        if (auth()->user()->isSuperAdmin()) {
+            return redirect()->route("dashboard.banking.beneficiaries.index")->with([
+                'status' => 'success',
+                'message' => 'The beneficiary updated successfully.',
+            ]);
+        }
+
         return redirect()->route("dashboard.banking.beneficiaries.index", ['filter' => ['workspace_id' => $beneficiary->workspace_id]])->with([
             'status' => 'success',
             'message' => 'The beneficiary updated successfully.',
@@ -153,7 +160,12 @@ class BeneficiaryController extends Controller
         $beneficiary->delete();
 
         event(new ContactDeleted($beneficiary));
-
+        if (auth()->user()->isSuperAdmin()) {
+            return redirect()->route("dashboard.banking.beneficiaries.index")->with([
+                'status' => 'success',
+                'message' => 'The beneficiary deleted successfully.',
+            ]);
+        }
         return redirect()->route("dashboard.banking.beneficiaries.index", ['filter' => ['workspace_id' => $beneficiary->workspace_id]])->with([
             'status' => 'success',
             'message' => 'The beneficiary deleted successfully.',
